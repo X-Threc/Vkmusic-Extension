6981a050801bfcf23ac7b3173ec99bd6bbc7c28c
X-Threc <79155566+X-Threc@users.noreply.github.com>
2022-10-02 11:37:02 +0300

    add redirect and delete unnecessary part

--- extension/script.js
+++ extension/script.js
@@ -1,40 +1,43 @@
-alert("FFFFFFFFFFFFFF")
-window.onload = function () {
-    var url = String(document.location.pathname)
-    console.log(url)
-
-    window.onclick = function(){
+var mutationObserver = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
         var url = String(document.location.pathname)
-        console.log(url)
-        console.log(document.documentURI)
-    }
+
+        if (url == "/audios123285528") {
+            var parentDOM = document.getElementsByClassName("CatalogSection__my").item(0)
+            var childs = parentDOM.childNodes
+            if (childs.length == 4) {
+                del_1= childs[0]
+                del_2= childs[2] 
+                parentDOM.removeChild(del_1)
+                parentDOM.removeChild(del_2)
+            }
+        }
+    })
+})
+
+mutationObserver.observe(document.getElementById("wrap3"), {
+    attributes: true,
+    characterData: true,
+    childList: true,
+    subtree: true,
+    attributeOldValue: true,
+    characterDataOldValue: true
+});
 
 
-    alert("FFFFFFFFFFFFFF2")
-    console.log(document.location)
-    i=0;
-    while (i<10){
-        url = String(document.location.pathname)
-        console.log(url)
-        i=i+1;
 
-    }
-    console.log(url)
+window.onload = function () {
+    // var url = String(document.location.pathname)
+    // console.log(url)
+
+    // document.getElementById("wrap3").onload = function () {
+    //     var url = String(document.location.pathname)
+    //     console.log(url)
+    //     console.log(document.documentURI)
+    // }
+
     hreflink = String(document.getElementById("l_aud").querySelector('a').href)
     var arr = hreflink.split("audios")
-    console.log(arr[1])
-
-    if (url == "/audios123285528") {
-        alert("FFFFFFFFFFFFFF")
-    }
-    
-
-
-    document.getElementById("l_aud").querySelector('a').setAttribute("href", "/audios"+arr[1]+"?section=all")
-    var redi = document.getElementById("l_aud").querySelector('a')
-    // document.getElementById("l_aud").querySelector('a').setAttribute("href", "https://vk.com/audios123285528?section=all")
-    var a = document.getElementById("l_aud")
-    var urls = a.getElementsByTagName('a')
-    console.log(redi)
-};
+    document.getElementById("l_aud").querySelector('a').setAttribute("href", "/audios" + arr[1] + "?section=all")
+}
 
