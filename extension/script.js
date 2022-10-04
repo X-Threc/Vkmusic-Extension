4f99fd6b87f9bcb465561e412b9e40a60113041f
X-Threc <79155566+X-Threc@users.noreply.github.com>
2022-10-04 17:21:30 +0300

    Change version manifest and add compare

--- extension/script.js
+++ extension/script.js
@@ -5,7 +5,7 @@ var mutationObserver = new MutationObserver(function (mutations) {
         if (url == "/audios123285528") {
             var parentDOM = document.getElementsByClassName("CatalogSection__my").item(0)
             var childs = parentDOM.childNodes
-            if (childs.length == 4) {
+            if (childs.length == 4 || childs.length == 3) {
                 del_1= childs[0]
                 del_2= childs[2] 
                 parentDOM.removeChild(del_1)
